6fcbbb08d396af2a4d6b36b0ab748eb70b33a32c
Pierreg-dev <pierre.godino@hotmail.com>
2022-08-19 13:55:32 +0200

    GET route for exercises

--- server/API/get/getExercise.js
+++ server/API/get/getExercise.js
@@ -1,13 +1,17 @@
 const collections = require('../../collections');
 module.exports = (req, res) => {
-  collections.Exercise.find(req.body.filter ? req.body.filter : null)
+  collections.Exercise.find(
+    req.params.subChapterId
+      ? { subChapterId: req.params.subChapterId.slice(1) }
+      : {}
+  )
     .lean()
     .then((data) => res.status(200).send({ data }))
     .catch((error) => {
       console.error(error);
       res.status(500).send({
         errNo: 500,
-        errName: 'Error when retrieving Exercises',
+        errName: 'Error when retrieving data',
       });
     });
 };
