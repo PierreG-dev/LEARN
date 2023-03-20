dcc7a5cd176d08d4c9ce0d6f764f1b867f41f3dc
PierreG-dev <aniteck82@hotmail.fr>
2023-03-20 23:39:54 +0100

    added full course 'Algorithmie en PHP'

--- app/components/Cours/AlgorithmiePHP/Fonctions.jsx
+++ app/components/Cours/AlgorithmiePHP/Fonctions.jsx
@@ -1,8 +1,67 @@
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const Fonctions = () => {
-  return <h1>Fonctions</h1>;
+  return (
+    <div>
+      <h1>Les fonctions</h1>
+      <p>
+        En PHP, les fonctions sont des blocs de code réutilisables qui peuvent
+        prendre des paramètres en entrée et retourner une valeur en sortie.
+        Elles sont très utiles pour organiser et réutiliser du code, et pour
+        effectuer des tâches spécifiques de manière répétitive.
+      </p>
+      <article>
+        <h2>Fonctions</h2>
+        <p>
+          Les fonctions en PHP permettent de regrouper des instructions en un
+          bloc qui peut être appelé et exécuté à plusieurs reprises dans un
+          script. Cela facilite la réutilisation de code et permet d'améliorer
+          la lisibilité et la maintenance du code.
+        </p>
+        <p>
+          Pour définir une fonction en PHP, on utilise le mot-clé{" "}
+          <code>function</code>, suivi du nom de la fonction et de ses
+          paramètres entre parenthèses, et enfin de ses instructions entre des
+          accolades.
+        </p>
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`function maFonction($parametre1, $parametre2) {\n  // Instructions de la fonction\n  return $resultat;\n}`}
+        </SyntaxHighlighter>
+        <p>
+          Ici, la fonction <code>maFonction</code> prend deux paramètres,{" "}
+          <code>$parametre1</code> et <code>$parametre2</code>, et retourne la
+          variable <code>$resultat</code>. Notez que la variable{" "}
+          <code>$resultat</code> peut être de n'importe quel type.
+        </p>
+        <p>
+          Pour appeler une fonction, on utilise simplement son nom suivi des
+          valeurs des paramètres entre parenthèses. La fonction est alors
+          exécutée avec les valeurs des paramètres qui lui sont passées, et peut
+          retourner une valeur.
+        </p>
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$resultat = maFonction(5, 10);\necho $resultat; // affiche la valeur retournée par la fonction`}
+        </SyntaxHighlighter>
+        <p>
+          Il est également possible de définir des paramètres optionnels pour
+          une fonction en leur affectant une valeur par défaut. Si un appel à la
+          fonction ne fournit pas de valeur pour ces paramètres, leur valeur par
+          défaut sera utilisée.
+        </p>
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`function maFonction($parametre1, $parametre2 = 10) {\n  // Instructions de la fonction\n  return $resultat;\n}`}
+        </SyntaxHighlighter>
+        <p>
+          Ici, la fonction <code>maFonction</code> a deux paramètres,{" "}
+          <code>$parametre1</code> et <code>$parametre2</code>, avec une valeur
+          par défaut de 10. Ainsi, si un appel à la fonction ne fournit pas de
+          valeur pour <code>$parametre2</code>, sa valeur par défaut sera
+          utilisée.
+        </p>
+      </article>
+    </div>
+  );
 };
 
 export default Fonctions;
--- app/components/Cours/AlgorithmiePHP/Operateurs.jsx
+++ app/components/Cours/AlgorithmiePHP/Operateurs.jsx
@@ -1,5 +1,5 @@
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const Operateurs = () => {
   return (
@@ -13,108 +13,111 @@ const Operateurs = () => {
       <article>
         <h2>Opérateurs d'affectation</h2>
         <h3>Affectation simple</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x = 2 //Affecte à x la valeur 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x = 2; //Affecte à x la valeur 2`}
         </SyntaxHighlighter>
         <h3>Affectation après addition</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x += 2 //Affecte à x le résultat de l'addition entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x += 2; //Affecte à x le résultat de l'addition entre x et 2`}
         </SyntaxHighlighter>
         <h3>Affectation après soustraction</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x -= 2 //Affecte à x le résultat de la soustraction entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x -= 2; //Affecte à x le résultat de la soustraction entre x et 2`}
         </SyntaxHighlighter>
         <h3>Affectation après multiplication</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x *= 2 //Affecte à x le résultat de la multiplication entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x *= 2; //Affecte à x le résultat de la multiplication entre x et 2`}
         </SyntaxHighlighter>
         <h3>Affectation après division</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x /= 2 //Affecte à x le résultat de la division entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x /= 2; //Affecte à x le résultat de la division entre x et 2`}
         </SyntaxHighlighter>
         <h3>Affectation du reste</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x %= 2 //Affecte à x le reste de la division entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x %= 2; //Affecte à x le reste de la division entre x et 2`}
         </SyntaxHighlighter>
         <h3>Affectation après exponentiation</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x **= 2 //Affecte à x le résultat de l'exponentiation entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x **= 2; //Affecte à x le résultat de l'exponentiation entre x et 2`}
         </SyntaxHighlighter>
         <h3>Incrémentation</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x++ //Affecte à x sa valeur + 1`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x++; //Affecte à x sa valeur + 1`}
         </SyntaxHighlighter>
         <h3>Décrémentation</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x-- //Affecte à x sa valeur - 1`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x--; //Affecte à x sa valeur - 1`}
         </SyntaxHighlighter>
       </article>
+
       <article>
-        <h2>Opérations arithmétiques</h2>
+        <h2>Opérateurs arithmétiques</h2>
         <h3>Addition</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x + 2 //Retourne le résultat de l'addition entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x + 2; //Retourne le résultat de l'addition entre x et 2`}
         </SyntaxHighlighter>
         <h3>Soustraction</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x - 2 //Retourne le résultat de la soustraction entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x - 2; //Retourne le résultat de la soustraction entre x et 2`}
         </SyntaxHighlighter>
         <h3>Multiplication</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x * 2 //Retourne le résultat de la multiplication entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x * 2; //Retourne le résultat de la multiplication entre x et 2`}
         </SyntaxHighlighter>
         <h3>Division</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x / 2 //Retourne le résultat de la division entre x et 2`}
-        </SyntaxHighlighter>
-        <h3>Exponentiation</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x ** 2 //Retourne le résultat de l'exponentiation entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x / 2; //Retourne le résultat de la division entre x et 2`}
         </SyntaxHighlighter>
         <h3>Modulo</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`x % 2 //Retourne le reste de la division entre x et 2`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x % 2; //Retourne le reste de la division entre x et 2`}
+        </SyntaxHighlighter>
+        <h3>Exponentiation</h3>
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`$x ** 2; //Retourne le résultat de l'exponentiation entre x et 2`}
         </SyntaxHighlighter>
       </article>
+
       <article>
         <h2>Opérateurs de comparaison</h2>
         <h3>Egalité</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`1 == "1" //Retourne true\n1 == 2 //Retourne false`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`1 == "1"; //Retourne true\n1 == 2; //Retourne false`}
         </SyntaxHighlighter>
         <h3>Inégalité</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`1 != "1" //Retourne false\n1 != 2 //Retourne true`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`1 != "1"; //Retourne false\n1 != 2; //Retourne true`}
         </SyntaxHighlighter>
         <h3>Egalité stricte</h3>
         <p>
           Contrairement à l'égalité simple, l'égalité stricte compare aussi le
           type.
         </p>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`1 === "1" //Retourne false\n1 === 1 //Retourne true`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`1 === "1"; //Retourne false\n1 === 1; //Retourne true`}
         </SyntaxHighlighter>
         <h3>Inegalité stricte</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`1 !== "1" //Retourne true\n1 !== 1 //Retourne false`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`1 !== "1"; //Retourne true\n1 !== 1; //Retourne false`}
         </SyntaxHighlighter>
         <h3>Supériorité stricte</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`1 > 0 //Retourne true\n1 > 1 //Retourne false\n1 > 2 //Retourne false`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`1 > 0; //Retourne true\n1 > 1; //Retourne false\n1 > 2; //Retourne false`}
         </SyntaxHighlighter>
         <h3>Supériorité</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`1 >= 0 //Retourne true\n1 >= 1 //Retourne true\n1 >= 2 //Retourne false`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`1 >= 0; //Retourne true\n1 >= 1; //Retourne true\n1 >= 2; //Retourne false`}
         </SyntaxHighlighter>
         <h3>Infériorité stricte</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`1 < 0 //Retourne false\n1 < 1 //Retourne false\n1 < 2 //Retourne true`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`1 < 0; //Retourne false\n1 < 1; //Retourne false\n1 < 2; //Retourne true`}
         </SyntaxHighlighter>
         <h3>Infériorité</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`1 <= 0 //Retourne false\n1 <= 1 //Retourne true\n1 <= 2 //Retourne true`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`1 <= 0; //Retourne false\n1 <= 1; //Retourne true\n1 <= 2; //Retourne true`}
         </SyntaxHighlighter>
       </article>
+
       <article>
         <h2>Opérateurs de condition</h2>
         <p>
@@ -122,12 +125,12 @@ const Operateurs = () => {
           dans le but de faire une condition logique plus complexe.
         </p>
         <h3>ET logique</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`true && true //Retourne true\ntrue && false //Retourne false\nfalse && true //Retourne false\nfalse && false //Retourne false`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`true && true; //Retourne true\ntrue && false; //Retourne false\nfalse && true; //Retourne false\nfalse && false; //Retourne false`}
         </SyntaxHighlighter>
         <h3>OU logique</h3>
-        <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-          {`true || true //Retourne true\ntrue || false //Retourne true\nfalse || true //Retourne true\nfalse || false //Retourne false`}
+        <SyntaxHighlighter language="php" style={vscDarkPlus}>
+          {`true || true; //Retourne true\ntrue || false; //Retourne true\nfalse || true; //Retourne true\nfalse || false; //Retourne false`}
         </SyntaxHighlighter>
       </article>
     </div>
