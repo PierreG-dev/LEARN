f1b4cdce47c6e098f385744ca594c6420e8ca613
PierreG-dev <aniteck82@hotmail.fr>
2023-07-06 23:52:30 +0200

    login route updated, now supports the real auth system with accounts in the database

--- server/API/auth/login.js
+++ server/API/auth/login.js
@@ -1,25 +1,34 @@
+const collections = require("@collections");
+const { sha256 } = require("js-sha256");
 const jwt = require("jsonwebtoken");
-// Route d'authentification pour définir l'utilisateur dans la session
-module.exports = (req, res) => {
-  // Vérifiez les informations d'identification
-  const login = req.body.login;
-  const password = req.body.password;
 
-  if (
-    login === process.env.ADMIN_USERNAME &&
-    password === process.env.ADMIN_PASSWORD
-  ) {
-    const token = jwt.sign(
-      { username: login, roles: ["admin"] },
-      process.env.ENCRYPT_KEY
-    );
-    // retour positif + envoi du JWT
-    res.status(200).json({ code: 200, msg: "Connexion réussie", token: token });
-  } else {
-    // Retour négatif
-    res.status(401).json({
-      code: 401,
-      msg: "Identifiants invalides",
+module.exports = async (req, res) => {
+  const { login: _login, password: _password } = req.body;
+
+  if (!_login || !_password)
+    return res.status(400).send({
+      code: 400,
+      msg: "Login and password are required to proceed",
     });
-  }
+
+  const user = await collections.User.findOne({ login: _login });
+  const _hashedPassword = sha256(_password);
+
+  if (!user || _hashedPassword !== user.hashedPassword)
+    return res.status(400).send({
+      code: 400,
+      msg: "Invalid credentials",
+    });
+
+  const token = jwt.sign(
+    { id: user._id, roles: user.roles },
+    process.env.ENCRYPT_KEY,
+    { expiresIn: "12h" }
+  );
+
+  return res.status(200).send({
+    code: 200,
+    msg: "You are now connected",
+    token: token,
+  });
 };
