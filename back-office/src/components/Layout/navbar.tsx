692d3eab3ad2cbe362f4b3ff39b5f4197d949430
Pierreg-dev <pierre.godino@hotmail.com>
2023-10-23 16:02:52 +0200

    working on profile tag in navbar(in progress)

--- back-office/src/components/Layout/navbar.tsx
+++ back-office/src/components/Layout/navbar.tsx
@@ -1,21 +1,32 @@
-import { Link, useLocation } from "react-router-dom";
-import { MdLogout } from "react-icons/md";
-import { useSelector } from "react-redux";
-import { RootState } from "../../store/index.ts.ts";
-import { BsDot } from "react-icons/bs";
+import { Link, useLocation } from 'react-router-dom';
+import { MdLogout } from 'react-icons/md';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/index.ts.ts';
+import { BsDot } from 'react-icons/bs';
 
 interface Props {
   handleLogout: () => void;
 }
 
-const links = [
+const user_links = [
   {
-    link: "/classes",
-    name: "Classes",
+    link: '/courses',
+    name: 'Cours',
   },
   {
-    link: "/chapters",
-    name: "Chapitres",
+    link: '/exercices',
+    name: 'Exercices',
+  },
+];
+
+const admin_links = [
+  {
+    link: '/classes',
+    name: 'Classes',
+  },
+  {
+    link: '/chapters',
+    name: 'Chapitres',
   },
 ];
 
@@ -26,6 +37,7 @@ const Navbar = ({ handleLogout }: Props) => {
     (state: RootState) => state.globals.isServerOnline
   );
 
+  console.log(connectedUser?.avatarUrl);
   return (
     <nav>
       <div>
@@ -37,16 +49,31 @@ const Navbar = ({ handleLogout }: Props) => {
             </div>
             <h2>
               LEARN
-              {(connectedUser?.roles?.includes("teacher") ||
-                connectedUser?.roles?.includes("admin")) && <sup>Manager</sup>}
+              {(connectedUser?.roles?.includes('teacher') ||
+                connectedUser?.roles?.includes('admin')) && <sup>Manager</sup>}
             </h2>
           </Link>
 
-          <ul>
-            {links.map((link, key) => (
+          <ul id="user_navlinks">
+            {user_links.map((link, key) => (
               <li
                 className={
-                  location.pathname.includes(link.link) ? "selected" : ""
+                  location.pathname.includes(link.link) ? 'selected' : ''
+                }
+                key={key}
+              >
+                <Link to={link.link}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+
+          <div id="navbar_separator" />
+
+          <ul id="admin_navlinks">
+            {admin_links.map((link, key) => (
+              <li
+                className={
+                  location.pathname.includes(link.link) ? 'selected' : ''
                 }
                 key={key}
               >
@@ -56,14 +83,26 @@ const Navbar = ({ handleLogout }: Props) => {
           </ul>
         </div>
       </div>
-      <div>
-        <button onClick={handleLogout}>
-          <MdLogout />
-        </button>
+
+      <div id="profile_container">
+        <img src={connectedUser?.avatarUrl} alt={connectedUser?.username} />
+        <ul id="navbar_profile_selector">
+          <li>
+            <a href="/profile">Mon compte</a>
+          </li>
+          <li>
+            <a href="/settings">Paramètres</a>
+          </li>
+          <li value="" onClick={handleLogout}>
+            Se déconnecter
+            <MdLogout />
+          </li>
+        </ul>
+
         <BsDot
           id="connection_status"
-          title={isServerOnline ? "En ligne" : "Tentative de reconnexion..."}
-          style={{ color: isServerOnline ? "green" : "orangered" }}
+          title={isServerOnline ? 'En ligne' : 'Tentative de reconnexion...'}
+          style={{ color: isServerOnline ? 'green' : 'orangered' }}
         />
       </div>
     </nav>
